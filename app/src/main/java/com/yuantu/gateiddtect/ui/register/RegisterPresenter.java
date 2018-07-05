40e49bb1ad9d52d635f51c497d8fd615b1128c08
yanxujun <yanxujun@yuantutech.com>
2018-07-05 10:54:25 +0800

    mvp register

---
+++ app/src/main/java/com/yuantu/gateiddtect/ui/register/RegisterPresenter.java
@@ -0,0 +1,30 @@
+package com.yuantu.gateiddtect.ui.register;
+
+import android.graphics.Bitmap;
+
+import com.arcsoft.facerecognition.AFR_FSDKFace;
+import com.yuantu.gateiddtect.GateApp;
+import com.yuantu.gateiddtect.ui.BasePresenter;
+
+/**
+ * Author:  Yxj
+ * Time:    2018/7/5 上午10:42
+ * -----------------------------------------
+ * Description:
+ */
+public class RegisterPresenter implements BasePresenter {
+
+    RegisterView view;
+
+    public RegisterPresenter(RegisterView view) {
+        this.view = view;
+    }
+
+    public void save(String name, AFR_FSDKFace mAFR_FSDKFace, Bitmap bitmap) {
+        GateApp.instance.mFaceDB.addFace(name, mAFR_FSDKFace, bitmap);
+    }
+
+    public void update(long id, AFR_FSDKFace mAFR_FSDKFace, Bitmap bitmap) {
+        GateApp.instance.mFaceDB.updateFace(id, mAFR_FSDKFace, bitmap);
+    }
+}
---
+++ app/src/main/java/com/yuantu/gateiddtect/ui/register/RegisterView.java
@@ -0,0 +1,12 @@
+package com.yuantu.gateiddtect.ui.register;
+
+import com.yuantu.gateiddtect.ui.BaseView;
+
+/**
+ * Author:  Yxj
+ * Time:    2018/7/5 上午10:42
+ * -----------------------------------------
+ * Description:
+ */
+public interface RegisterView extends BaseView{
+}
